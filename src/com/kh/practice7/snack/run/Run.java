2a956da7219080ec4eb6855349489759ae7fb48a
jchj108 <jchj108@naver.com>
2021-03-08 22:14:38 +0900

    snack MVC first commit

---
+++ src/com/kh/practice7/snack/run/Run.java
@@ -0,0 +1,13 @@
+package com.kh.practice7.snack.run;
+
+import com.kh.practice7.snack.view.SnackMenu;
+
+public class Run {
+
+	public static void main(String[] args) {
+
+		SnackMenu sm = new SnackMenu();
+		sm.menu();
+	}
+
+}
