7477a79ab2a20be3618c5e28aca030409dca2ace
jchj108 <jchj108@naver.com>
2021-03-25 05:31:36 +0900

    int count = 0; 		for(int i = 0; i < bList.length; i++) { 			if(bList[i].getTitle().contains(keyword)) { 				tmp[count++] = bList[i]; 				// i는 포문을 돌 때마다 늘어나지만 count는 if문이 일치할 때만 늘어난다 			}

--- src/com/kh/homework/poly/controller/LibraryController.java
+++ src/com/kh/homework/poly/controller/LibraryController.java
@@ -1,28 +1,56 @@
 package com.kh.homework.poly.controller;
 
+import com.kh.homework.poly.model.vo.AniBook;
 import com.kh.homework.poly.model.vo.Book;
+import com.kh.homework.poly.model.vo.CookBook;
 import com.kh.homework.poly.model.vo.Member;
 
 public class LibraryController {
 
 	private Member mem = null;
 	private Book[] bList = new Book[5];
+	{
+	bList[0] = new CookBook("백종원의 집밥", "백종원", "tvN", true);
+	bList[1] = new AniBook("한번 더 해요", "미티", "원모어", 19);
+	bList[2] = new AniBook("루피의 원피스", "루피", "japan", 12);
+	bList[3] = new CookBook("이헤정의 얼마나 맛있게요", "이혜정", "문학", false);
+	bList[4] = new CookBook("최현석 날 따라해봐", "최현석", "소금책", true);
+	}
 	
 	public void insertMember(Member mem) {
-
-		
+		this.mem = mem;
+	}
+	
+	
+	public Member myInfo() {
+		return mem;
 	}
 	
 	public Book[] selectAll() {
-		
+		return bList;
 	}
 	
-	public Book[] searchBook(String name) {
+	public Book[] searchBook(String keyword) {
+		Book[] tmp = new Book[bList.length];
 		
+		int count = 0;
+		for(int i = 0; i < bList.length; i++) {
+			if(bList[i].getTitle().contains(keyword)) {
+				tmp[count++] = bList[i];
+				// i는 포문을 돌 때마다 늘어나지만 count는 if문이 일치할 때만 늘어난다
+			}
+		}
+		return tmp;
 	}
+					
+					
 	
 	public int rentBook(int index) {
+		int result = 0;
+		if 
 		
+		
+		return result;
 	}
 	
 	
--- src/com/kh/homework/poly/view/LibraryMenu.java
+++ src/com/kh/homework/poly/view/LibraryMenu.java
@@ -3,13 +3,14 @@ package com.kh.homework.poly.view;
 import java.util.Scanner;
 
 import com.kh.homework.poly.controller.LibraryController;
+import com.kh.homework.poly.model.vo.Book;
 import com.kh.homework.poly.model.vo.Member;
 
 public class LibraryMenu {
 
 	private LibraryController lc = new LibraryController();
 	private Scanner sc = new Scanner(System.in);
-	
+
 	public void mainMenu() {
 		System.out.print("이름 : ");
 		String name = sc.nextLine();
@@ -17,28 +18,57 @@ public class LibraryMenu {
 		int age = Integer.parseInt(sc.nextLine());
 		System.out.print("성별 : ");
 		char gender = sc.nextLine().charAt(0);
-		
+
 		Member mem = new Member(name, age, gender);
 		lc.insertMember(mem);
 		int menu = 0;
-		
-		while (menu!=9) {
-			
+
+		while (menu != 9) {
+
+			System.out.println("1. 마이페이지");
+			System.out.println("2. 도서 전체 조회");
+			System.out.println("3. 도서 검색");
+			System.out.println("4. 도서 대여하기");
+			System.out.println("9. 프로그램 종료하기");
+			System.out.print("메뉴 번호 : ");
+			menu = sc.nextInt();
+			switch (menu) {
+			case 1:
+				lc.myInfo();
+				break;
+			case 2:
+				lc.selectAll();
+				break;
+			case 3:
+				lc.searchBook();
+				break;
+			case 4:
+				lc.rentBook(index);
+				break;
+			case 9:
+				break;
+			default:
+				System.out.println("잘못 입력하셨습니다.");
+			}
 		}
-		
 	}
-	
+
 	public void selectAll() {
-		
+		Book[] bList = lc.selectAll();
+		for(int i = 0; i< bList.length; i++) {
+			System.out.println(i + "번도서 : " + bList[i]);
+		}
 	}
-	
+
 	public void searchBook() {
+		System.out.print("검색할 제목 키워드 : ");
+		String keyword = sc.nextLine();
+		lc.searchBook(keyword);
 		
 	}
-	
+
 	public void rentBook() {
-		
+
 	}
-	
-	
+
 }
