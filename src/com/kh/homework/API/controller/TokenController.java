1abc15c947090e929814f106fc219a6634970465
jchj108 <jchj108@naver.com>
2021-04-08 15:21:12 +0900

    api StringTokenizer

--- src/com/kh/homework/API/controller/TokenController.java
+++ src/com/kh/homework/API/controller/TokenController.java
@@ -1,10 +1,36 @@
 package com.kh.homework.API.controller;
 
+import java.util.StringTokenizer;
+
 public class TokenController {
 
+	public String afterToken(String str) {
+		StringTokenizer st = new StringTokenizer(str, " ");
+		String noToken = "";
+		while (st.hasMoreTokens()) {
+			noToken += st.nextToken();
+		}
+		return noToken;
+	}
+
+	public String firstCap(String input) {
+		char cap = input.toUpperCase().charAt(0);
+		String result = "";
+		result += cap;
+		for (int i = 1; i < input.length(); i++) {
+			result += input.charAt(i);
+		} 
+		return result;
+	}
 	
-	public String afterToken()
-	
-	public 
-	
+	public int findChar(String input, char one) {
+		int count = 0;
+		for(int i = 0; i < input.length(); i++) {
+			if(one == input.charAt(i)) {
+				count++;
+			}
+		}
+		return count;
+	}
+
 }
--- src/com/kh/homework/API/run/Run.java
+++ src/com/kh/homework/API/run/Run.java
@@ -1,9 +1,14 @@
 package com.kh.homework.API.run;
 
+import com.kh.homework.API.view.TokenMenu;
+
 public class Run {
 
 	public static void main(String[] args) {
-
+		
+		
+		new TokenMenu().mainMenu();
+		
 	}
 
 }
